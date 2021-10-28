8a9fde6cfc3d249a20a8ffd1163ab4c1bf47288f
jurgendominicpacana <201980016@psu.palawan.edu.ph>
2021-10-29 02:14:04 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3630829edcaf5849876520a124d907ca869122b0',
+        'name' => 'jurgendominicpacana/logapp-pacana',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jurgendominicpacana/logapp-pacana' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3630829edcaf5849876520a124d907ca869122b0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
