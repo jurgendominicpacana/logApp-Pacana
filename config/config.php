54367800d6704fe9ec50efdd1d99c19fd30fa641
jurgendominicpacana <201980016@psu.palawan.edu.ph>
2021-10-28 21:53:42 +0800

    from logApp-scaling-octo

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/logApp-scaling-octo');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'suckmuckfuck1');
+	define('DB_NAME', 'guestlist');
